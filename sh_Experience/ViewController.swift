bf42e3df8b104a7002a0c8613fe6f703b4627bc1
이성훈 <LeeSungHun@iseonghun-ui-macbook-pro.local>
2018-05-16 11:02:46 +0900

    계산 함수 등록

--- sh_Experience/ViewController.swift
+++ sh_Experience/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var sum = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var count = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        print("총 합은 \(self.sum)입니다.\(count)번 합했네요")
     }
 
     override func didReceiveMemoryWarning() {
